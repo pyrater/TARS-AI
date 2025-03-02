f0633fe53bf1652bcc262798b2e5d4d943a26f08
pyrater <pyrater@gmail.com>
2025-03-02 07:24:20 -0700

    tweaks to install script

--- Install.sh
+++ Install.sh
@@ -29,7 +29,8 @@ sudo apt clean
 sudo apt update -y
 
 # Install necessary dependencies
-sudo apt install -y chromium-browser chromium-chromedriver sox libsox-fmt-all portaudio19-dev espeak-ng xterm
+sudo apt install -y chromium-browser chromium-chromedriver sox libsox-fmt-all portaudio19-dev espeak-ng --fix-missing
+sudo apt install -y xterm --fix-missing
 
 # Verify installations
 chromium-browser --version
@@ -69,16 +70,17 @@ retry_pip_install
 # Copy configuration files if they do not exist
 if [ ! -f "config.ini" ]; then
     cp config.ini.template config.ini
-    sudo chmod 777 config.ini
     sudo chown $USER:$USER config.ini
+    sudo chmod 644 config.ini
     echo "Default config.ini created. Please edit it with necessary values."
 fi
 
 if [ ! -f "../.env" ]; then
     cp ../.env.template ../.env
-    sudo chmod 777 ../.env
-    sudo chown $USER:$USER config.ini
+    sudo chown $USER:$USER ../.env
+    sudo chmod 644 ../.env
     echo "Default .env created. Please edit it with necessary values."
 fi
+
 
 echo "Installation completed successfully!"
